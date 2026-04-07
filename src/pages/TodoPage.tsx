6ebda64832cdbdd4a5c1fecaef8f7fbffa0809c3
PhamTuanDat <pdat100111@gmail.com>
2026-04-07 17:10:56 +0700

    feat: logic cta login when user not logged in

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,61 +1,60 @@
 import { useEffect, useState } from "react";
-import { Card, message } from "antd";
+import { Card, message, Typography, Button, Space } from "antd";
 import TodoInput from "components/todo/TodoInput";
 import TodoFilter from "components/todo/TodoFilter";
 import TodoList from "components/todo/TodoList";
 import { supabase } from "@/utils/supabase";
+import { LoginOutlined } from "@ant-design/icons";
+import { Link, useNavigate } from "react-router";
+import { useCurrentApp } from "@/context/app.context";
+
+const { Title, Text } = Typography;
 
 type Todo = {
   id: number;
   title: string;
   completed: boolean;
 };
+
 const TodoPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filter, setFilter] = useState("all");
+  const navigate = useNavigate();
+  const { isAuthenticated, user } = useCurrentApp();
 
   useEffect(() => {
-    async function fetchTodos() {
-      const { data, error } = await supabase.from("todos").select("*").order("id", { ascending: false });
+    if (isAuthenticated && user) {
+      async function fetchTodos() {
+        const { data, error } = await supabase.from("todos").select("*").order("id", { ascending: false }).eq("created_by", user.id);
 
-      if (error) message.error("Không thể tải dữ liệu!");
-      else setTodos(data || []);
+        if (error) message.error("Không thể tải dữ liệu!");
+        else setTodos(data || []);
+      }
+      fetchTodos();
     }
-
-    fetchTodos();
-  }, []);
+  }, [isAuthenticated, user]);
 
   async function addTodo(title: string) {
     if (!title.trim()) return;
-
     const { data, error } = await supabase
       .from("todos")
       .insert([{ title: title, completed: false }])
       .select();
 
-    if (error) {
-      message.error("Lỗi khi thêm mới!");
-    } else {
-      setTodos([data[0], ...todos]);
-    }
+    if (error) message.error("Lỗi khi thêm mới!");
+    else setTodos([data[0], ...todos]);
   }
 
   async function toggleTodo(id: number, currentStatus: boolean) {
     const { error } = await supabase.from("todos").update({ completed: !currentStatus }).eq("id", id);
-
-    if (error) {
-      message.error("Không thể cập nhật!");
-    } else {
-      setTodos(todos.map((t) => (t.id === id ? { ...t, completed: !currentStatus } : t)));
-    }
+    if (error) message.error("Không thể cập nhật!");
+    else setTodos(todos.map((t) => (t.id === id ? { ...t, completed: !currentStatus } : t)));
   }
 
   async function deleteTodo(id: number) {
     const { error } = await supabase.from("todos").delete().eq("id", id);
-
-    if (error) {
-      message.error("Lỗi khi xoá!");
-    } else {
+    if (error) message.error("Lỗi khi xoá!");
+    else {
       setTodos(todos.filter((t) => t.id !== id));
       message.success("Đã xoá công việc");
     }
@@ -66,6 +65,31 @@ const TodoPage = () => {
     if (filter === "completed") return t.completed;
     return true;
   });
+
+
+  if (!isAuthenticated) {
+    return (
+      <div style={{ textAlign: "center", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Space orientation="vertical" size="large">
+          <Title level={1} style={{ fontSize: "64px", marginBottom: 0 }}>
+            TODO APP
+          </Title>
+          <Text type="secondary" style={{ fontSize: "18px" }}>
+            Vui lòng <Link to="/login">đăng nhập</Link> để quản lý công việc của bạn hiệu quả hơn.
+          </Text>
+          <Button
+            type="primary"
+            size="large"
+            icon={<LoginOutlined />}
+            onClick={() => navigate("/login")}
+            style={{ height: "50px", padding: "0 40px", borderRadius: "8px" }}
+          >
+            Đăng nhập ngay
+          </Button>
+        </Space>
+      </div>
+    );
+  }
 
   return (
     <div style={{ maxWidth: 600, margin: "50px auto" }}>
