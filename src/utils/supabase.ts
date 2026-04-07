9a2d8426004197ef03a27d8eb0343bc874cce696
PhamTuanDat <pdat100111@gmail.com>
2026-04-07 17:08:32 +0700

    feat: function signupUser and loginUser with supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,3 +4,28 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const signUpUser = async (email: string, password: string, username: string) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        username: username,
+        avatar_url: `https://api.dicebear.com/7.x/miniavs/svg?seed=${username}`,
+      },
+    },
+  });
+  if (error) throw error;
+  return data;
+};
+
+export const signInUser = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw error;
+  return data;
+};
+
